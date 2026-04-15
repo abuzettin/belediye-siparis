a25fb15e939b28939bb7d8baf8a762df5219fcc1
abuzettin <61846213+abuzettin@users.noreply.github.com>
2026-04-15 16:59:14 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
-import App from './App'
+import CafeOrderDisplay from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <CafeOrderDisplay />
   </React.StrictMode>,
 )
