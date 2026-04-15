7db5b368f49b8b669f2c31252c565fc69a52f952
abuzettin <61846213+abuzettin@users.noreply.github.com>
2026-04-15 16:52:25 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
 import React, { useMemo, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Bell, CheckCircle2, Clock3, Plus, Printer, Trash2 } from 'lucide-react';
+  );
+}
