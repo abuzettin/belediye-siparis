e1c795cd6885a440166c95624b1f281597493f56
abuzettin <61846213+abuzettin@users.noreply.github.com>
2026-04-15 16:14:26 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import "./App.css";
+
 export default function App() {
   return (
     <div style={{ padding: 40 }}>
       <h1>Belediye Sipariş Sistemi</h1>
       <p>Sistem başarıyla çalışıyor 🚀</p>
     </div>
-  )
+  );
 }
