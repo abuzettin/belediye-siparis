e246231b423c9cdc9d3a14bad22281228d32407e
abuzettin <61846213+abuzettin@users.noreply.github.com>
2026-04-15 16:05:55 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Belediye Sipariş Sistemi</h1>
+      <p>Sistem başarıyla çalışıyor 🚀</p>
+    </div>
+  )
+}
