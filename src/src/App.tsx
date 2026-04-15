91803bf6b2e111d9f1dcae50b2abace81eed44d5
abuzettin <61846213+abuzettin@users.noreply.github.com>
2026-04-15 13:50:02 +0300

    Update App.tsx

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,6 +1,8 @@
+import './App.css'
+
 export default function App() {
   return (
-    <div style={{ padding: 40, fontFamily: 'Arial' }}>
+    <div style={{ padding: 40 }}>
       <h1>Belediye Sipariş Sistemi</h1>
       <p>Sistem başarıyla çalışıyor 🚀</p>
     </div>
